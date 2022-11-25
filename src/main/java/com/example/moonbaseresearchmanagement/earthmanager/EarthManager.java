93201de832780f4b661addba6f5bce7b1e6d0d22
Felipe Gebara <feagmoreira@hotmail.com>
2022-11-25 16:44:46 -0700

    more updtates
    more updates

--- src/main/java/com/example/moonbaseresearchmanagement/earthmanager/EarthManager.java
+++ src/main/java/com/example/moonbaseresearchmanagement/earthmanager/EarthManager.java
@@ -9,8 +9,24 @@ import jakarta.persistence.*;
 @Table(name = "earth_manager")
 public class EarthManager extends Personnel {
     
-    
     @ManyToOne
     @JoinColumn(name = "station_name")
     private GroundStation station_name;
+
+    public EarthManager(int personnel_id, String name, String country, GroundStation station_name) {
+        super(personnel_id, name, country);
+        this.station_name = station_name;
+    }
+    
+    public GroundStation getStation_name() {
+        return station_name;
+    }
+
+    public void setStation_name(GroundStation station_name) {
+        this.station_name = station_name;
+    }
+
+  
+
+    
 }
