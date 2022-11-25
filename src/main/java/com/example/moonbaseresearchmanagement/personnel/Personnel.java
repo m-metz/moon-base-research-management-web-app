91cc5abf80102b48ab9ae2922e524e8fd97150ea
Felipe Gebara <feagmoreira@hotmail.com>
2022-11-25 14:55:04 -0700

    Personel and MoonResearcher intial
    initial mappings

--- src/main/java/com/example/moonbaseresearchmanagement/personnel/Personnel.java
+++ src/main/java/com/example/moonbaseresearchmanagement/personnel/Personnel.java
@@ -7,10 +7,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "personnel")
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Personnel implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
