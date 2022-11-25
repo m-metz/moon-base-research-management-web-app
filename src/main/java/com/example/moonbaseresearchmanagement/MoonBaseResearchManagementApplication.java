335b4666ea50248c7fa66c2a1cd56f3714b5fbef
Michael Metz <6386318+m-metz@users.noreply.github.com>
2022-11-25 12:26:07 -0700

    Check in springboot project template

---
+++ src/main/java/com/example/moonbaseresearchmanagement/MoonBaseResearchManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.moonbaseresearchmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonBaseResearchManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoonBaseResearchManagementApplication.class, args);
+	}
+
+}
