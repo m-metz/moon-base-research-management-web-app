22b8da661bd73eb0dc8f5cc691c7174168ac5833
AlexanderKruger <alexander.kruger@ucalgary.ca>
2022-11-27 00:00:42 -0700

    Adding Base Controller again

---
+++ src/main/java/com/example/moonbaseresearchmanagement/base/BaseController.java
@@ -0,0 +1,16 @@
+package com.example.moonbaseresearchmanagement.base;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping(path = "api/v1/moonbase")
+public class BaseController {
+
+   // private final BaseService baseService;
+    
+    
+}
