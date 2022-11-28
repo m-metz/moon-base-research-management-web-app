86cc30fa900425ad02e011941af6fc68f1a1a082
Felipe Gebara <feagmoreira@hotmail.com>
2022-11-27 19:19:05 -0700

    project complete
    project complete

--- src/main/java/com/example/moonbaseresearchmanagement/project/ProjectController.java
+++ src/main/java/com/example/moonbaseresearchmanagement/project/ProjectController.java
@@ -1,5 +1,25 @@
 package com.example.moonbaseresearchmanagement.project;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/project" )
 public class ProjectController {
+    private final ProjectService projectService;
+
+    @Autowired
+    public ProjectController(ProjectService projectService) {
+        this.projectService = projectService;
+    }
+
+    @GetMapping
+    public List<Project> getProjects() {
+        return projectService.getAllProjects();
+    }
     
 }
